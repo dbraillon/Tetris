0127258fa5f0884840b75cc1db0bea0609941426
Davy Braillon <davy.braillon@gmail.com>
2013-02-06 17:57:09 +0100

    v.1.0.1
    - Add MenuView, PlayView
    - Clean sources

---
+++ src/com/dbraillon/tetris/views/MenuView.java
@@ -0,0 +1,85 @@
+package com.dbraillon.tetris.views;
+
+import org.lwjgl.input.Keyboard;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
+
+import com.dbraillon.tetris.Game;
+
+public class MenuView {
+
+	private final int PLAY_ITEM = 0;
+	private final int QUIT_ITEM = 1;
+	
+	private final int GO_UP = -1;
+	private final int GO_DOWN = 1;
+	
+	
+	private int _width, _height;
+	private int _itemSelected;
+	
+	public MenuView(int width, int height) {
+		
+		_width = width;
+		_height = height;
+		_itemSelected = PLAY_ITEM;
+	}
+	
+	public void render(Graphics graphics) {
+		
+		graphics.drawString("Tétris2d", percent(_width, 10), percent(_height, 10));
+		graphics.drawString(get_ItemSelected(PLAY_ITEM) + " Jouer", percent(_width, 10), percent(_height, 10) + 20);
+		graphics.drawString(get_ItemSelected(QUIT_ITEM) + " Quitter", percent(_width, 10), percent(_height, 10) + 35);
+	}
+	
+	public int update(GameContainer gameContainer) {
+		
+		Input input = gameContainer.getInput();
+		
+		if(input.isKeyPressed(Keyboard.KEY_DOWN)) {
+			
+			change_ItemSelected(GO_DOWN);
+		}
+		else if(input.isKeyPressed(Keyboard.KEY_UP)) {
+			
+			change_ItemSelected(GO_UP);
+		}
+		else if(input.isKeyPressed(Keyboard.KEY_RETURN)) {
+			
+			return select_ItemSelected(gameContainer);
+		}
+		
+		return 0;
+	}
+	
+	private int percent(int value, int percent) {
+		
+		return value * percent / 100;
+	}
+	
+	private String get_ItemSelected(int value) {
+		
+		return (_itemSelected == value) ? ">" : " ";
+	}
+	 
+	private void change_ItemSelected(int value) {
+		
+		_itemSelected += value;
+		_itemSelected = (_itemSelected == 2) ? PLAY_ITEM : _itemSelected;
+		_itemSelected = (_itemSelected == -1) ? QUIT_ITEM : _itemSelected;
+	}
+	
+	private int select_ItemSelected(GameContainer gameContainer) {
+		
+		switch(_itemSelected) {
+		case PLAY_ITEM:
+			return Game.PLAY_VIEW;
+		case QUIT_ITEM:
+			gameContainer.exit();
+			break;
+		}
+		
+		return Game.MENU_VIEW;
+	}
+}
---
+++ src/com/dbraillon/tetris/views/PlayView.java
@@ -0,0 +1,31 @@
+package com.dbraillon.tetris.views;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+
+public class PlayView {
+
+	private final int HEIGHT_MAP = 22;
+	private final int WIDTH_MAP = 10;
+	
+	private final int SIZE_CLIP = 32;
+	
+	
+	private int _heightScreen, _widthScreen;
+	
+	
+	public PlayView(int width, int height) {
+		
+		_heightScreen = width;
+		_widthScreen = height;
+	}
+	
+	
+	public void render(Graphics graphics) {
+		
+	}
+	
+	public void update(GameContainer gameContainer) {
+		
+	}
+}
