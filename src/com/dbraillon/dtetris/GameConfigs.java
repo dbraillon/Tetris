22bd06e4afaaf2d04e8c7e8097aa272ece7f23ad
Davy Braillon <d.braillon@outlook.com>
2014-01-31 08:23:37 +0100

    Change in GameConfigs

--- src/com/dbraillon/dtetris/GameConfigs.java
+++ src/com/dbraillon/dtetris/GameConfigs.java
@@ -4,6 +4,6 @@ public class GameConfigs {
 
 	public final static int windowsWidth = 600;
 	public final static int windowsHeight = 800;
-	public final static String windowsName = "Tetris";
+	public final static String windowsName = "dTetris";
 	public final static int windowsFrameRate = 30;
 }
